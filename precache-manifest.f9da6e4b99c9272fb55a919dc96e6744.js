1be5deee8af60ee59a582138ef2b2608b72029c5
Yan <rotman.yan@gmail.com>
2019-04-04 02:34:13 +0300

    create

---
+++ precache-manifest.f9da6e4b99c9272fb55a919dc96e6744.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b37e09f47e8489658b18",
+    "url": "/static/js/main.eb810801.chunk.js"
+  },
+  {
+    "revision": "e2d5421afffcc7657ada",
+    "url": "/static/js/2.cac55842.chunk.js"
+  },
+  {
+    "revision": "b37e09f47e8489658b18",
+    "url": "/static/css/main.f6b69c4a.chunk.css"
+  },
+  {
+    "revision": "c71032ae254229aa72596e9cf2bae402",
+    "url": "/index.html"
+  }
+];
